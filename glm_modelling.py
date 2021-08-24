f2e87525d716d8ffbce0ce493230dbb08f0742d1
ClionaOD <odoherc1@tcd.ie>
2021-08-24 16:52:29 +0000

    include option to segment event

--- glm_modelling.py
+++ glm_modelling.py
@@ -3,6 +3,7 @@
 # Important first step for extracting betas which we will go on to use for MVPA
 
 import os
+from numpy.lib.shape_base import split
 import pandas as pd
 import pickle
 import numpy as np
@@ -10,27 +11,30 @@ import random
 
 from nilearn import surface
 from nilearn.glm.first_level import make_first_level_design_matrix, FirstLevelModel, run_glm
+from scipy.linalg.special_matrices import tri
 from scipy.sparse.construct import rand
 
-def load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns,t_r=0.656,slice_time_ref=0.5, remap_trial_types=None, elantags=False, conditions=None):
+def load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns,t_r=0.656,slice_time_ref=0.5, remap_trial_types=None, elantags=False, conditions=None, segment_into=None, mark_reps=False):
     """
     Load BIDS data, set up volume and surface regression models and estimate them
     
     args:
-    subjind     for which subject to process
-    bidsroot    path to bids folder
-    derived root    path to preprocessed outputs
-    taskname    name of the task as per the bids event descriptions
-    numruns     number of runs for task as named by taskname
+        subjind     for which subject to process
+        bidsroot    path to bids folder
+        derived root    path to preprocessed outputs
+        taskname    name of the task as per the bids event descriptions
+        numruns     number of runs for task as named by taskname
     
     t_r     is volume repetition time in seconds    
     slice_time_ref  point in volume slice timing is corrected to (percentage of t_r can have value between 0 and 1). Is 0.5 if fmriprep was used
+    
     remap_trial_types - dict of form {'remap_from' : 'remap_to'} for use if finer labelling (e.g. elan tagging) is to be used
     elantags, conditions    for remapping if remap_trial_types is not None
+    segment_into       number of seconds to split the event into. Set to None for keeping the intact event
 
     returns:
-    fmri_glm    the volume model in MNI152NLin2009cAsym space 
-    surf_glm    the surface model in fsaverage space
+        fmri_glm    the volume model in MNI152NLin2009cAsym space 
+        surf_glm    the surface model in fsaverage space
     """
     # This folder structure is based on fmriprep outputs
     subjpth=f'sub-{subjind}/ses-001/func'
@@ -60,6 +64,8 @@ def load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns,t_r=0.656,sl
 
         # Get rid of fixation events
         dfsess=dfsess[~(dfsess['trial_type'].str.startswith('fixation'))]
+        # Get rid of dummy events
+        dfsess=dfsess[~(dfsess['trial_type'].str.startswith('dummy'))]
 
         # Apply any requested remappings to trial_type
         dfsess['trial_type'] = dfsess['trial_type'].replace(remap_trial_types)
@@ -89,6 +95,49 @@ def load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns,t_r=0.656,sl
             #elan_df = elan_df[['onset', 'duration', 'trial_type']]
             dfsess = elan_df.copy()
             a = 1
+
+        if segment_into is not None:
+            segmented_df = pd.DataFrame(columns=['onset','duration','trial_type'])
+            for idx, row in dfsess.iterrows():
+                seg = {'onset':[], 'duration':[],'trial_type':[]}
+                split_event = np.arange(row.onset, row.onset+row.duration, segment_into)
+                seg['onset'] = list(split_event)
+
+                durs = list(np.diff(split_event))
+                durs.append(row.duration % segment_into)
+                
+                seg['duration']=durs
+                
+                seg['trial_type']=([f'{row.trial_type}_segment-{i}' for i in range(len(split_event))])
+
+                segmented_df = segmented_df.append(pd.DataFrame.from_dict(seg), ignore_index=True)
+            dfsess = segmented_df.copy()
+        
+        if mark_reps:
+            trialtypecount = {}
+            for idx, row in dfsess.iterrows():
+                tt=row.trial_type
+                if not tt in trialtypecount:
+                    trialtypecount[tt]=0
+                else:
+                    trialtypecount[tt]+=1 
+                dfsess.loc[idx,'trial_type'] += f'_rep-{trialtypecount[tt]}'
+            
+                # TODO: make this not crash when we generalise
+                orders_on = True
+                if orders_on:
+                    orders = pd.read_csv('expt_history_pilot_2.csv', index_col=0)
+                    orders = orders[orders['participantID'] == int(subjind)].orders.to_list()
+                    orders = orders[0].split('-')
+
+                    if trialtypecount[tt] == 0:
+                        dfsess.loc[idx,'trial_type'] += f'_order-{orders[0]}'
+                    elif trialtypecount[tt] == 1:
+                        dfsess.loc[idx,'trial_type'] += f'_order-{orders[1]}'
+                    elif trialtypecount[tt] == 2:
+                        dfsess.loc[idx,'trial_type'] += f'_order-{orders[2]}'
+                    else:
+                        raise ValueError('the number of repetitions does not match expt history')
 
         # Add this run
         events.append(dfsess)
@@ -125,18 +174,18 @@ def load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns,t_r=0.656,sl
 if __name__ == '__main__':
     
     # Set paths for saving
-    modelpth = '/home/CUSACKLAB/clionaodoherty/foundcog-adult-pilot-2-analysis/remap/models'
+    modelpth = '/home/CUSACKLAB/clionaodoherty/foundcog-adult-pilot-2-analysis/segmented/models'
 
     # Set arguments for glm fitting
-    #subjects = list(range(2,19))
-    subjects=[18]
+    subjects = list(range(2,18))
+    #subjects=[18]
     subjects = [f'{subjind:02}' for subjind in subjects]
     
     bidsroot = '/home/CUSACKLAB/clionaodoherty/foundcog-adult-pilot-2-analysis/bids'
     derivedroot = '/home/CUSACKLAB/clionaodoherty/foundcog-adult-pilot-2-analysis/deriv-2_topup'
 
-    taskname = 'pictures'
-    numruns = 3
+    taskname = 'video'
+    numruns = 2
 
     tasklist = {
                 'pictures':{
@@ -153,7 +202,7 @@ if __name__ == '__main__':
                 'video':{
                         'numruns':2,
                         'trial_types':[ 'bathsong.mp4', 'dog.mp4', 'new_orleans.mp4', 'minions_supermarket.mp4', 'forest.mp4', 'piper.mp4'],
-                        'n_reps':1
+                        'n_reps':3
                         }
                 }
 
@@ -165,8 +214,13 @@ if __name__ == '__main__':
                         'dog.mp4':['dog','cat', 'ball', 'fence'], 
                         'piper.mp4':['seabird','crab', 'fish', 'seashell']}
     
-    remap_pictures = True
-    remap_shuffle = True
+    remap_pictures = False
+    remap_shuffle = False
+
+    # Number of seconds to segment movies into. Set to None for intact movies
+    segment_into = 3.75 
+    mark_reps = True
+
     # Pictures will just be represented by their corresponding video this after remapping
     if remap_pictures:
         tasklist['pictures']['trial_types'] = tasklist['video']['trial_types']
@@ -189,15 +243,17 @@ if __name__ == '__main__':
             remap='_remap-shuffle'
         
         remap_pictures_to_video = {z:x for x,y in pic_vid_mapping.items() for z in y}
+    else:
+        remap=''
 
     for subjind in subjects:
         print(f'working on subject {subjind}')
         if remap_pictures:
-            fmri_glm, surf_glm = load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns, remap_trial_types=remap_pictures_to_video)
+            fmri_glm, surf_glm = load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns, remap_trial_types=remap_pictures_to_video, segment_into=segment_into, mark_reps=mark_reps)
         else:
-            fmri_glm, surf_glm = load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns)
+            fmri_glm, surf_glm = load_and_estimate(subjind,bidsroot,derivedroot,taskname,numruns, segment_into=segment_into, mark_reps=mark_reps)
         
         os.makedirs(os.path.join(modelpth,f'sub-{subjind}'), exist_ok=True)
         
-        with open(os.path.join(modelpth,f'sub-{subjind}',f'sub-{subjind}_task-{taskname}{remap}_models.pickle'),'wb') as f:
-                pickle.dump({'fmri_glm': fmri_glm, 'surf_glm': surf_glm},f)
+        with open(os.path.join(modelpth,f'sub-{subjind}',f'sub-{subjind}_task-{taskname}{remap}_segment-{segment_into}_models.pickle'),'wb') as f:
+            pickle.dump({'fmri_glm': fmri_glm, 'surf_glm': surf_glm},f)
